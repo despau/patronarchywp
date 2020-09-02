b9017c98cd79e11909e2e9da62cb6aa8bde97314
raug-dev24 <romanegustin24@gmail.com>
2020-09-02 00:02:27 -0400

    adding header and footer

---
+++ footer.php
@@ -0,0 +1,6 @@
+
+
+        <h1>Footer.php</h1>
+    <php wp_foot; ?>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -1,11 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>WP Patronarchy</title>
-</head>
-<body>
-    <h1>Index.php</h1>
-</body>
-</html>
+<?php get_header(); ?>
+
+    <div>Index.php</div>
+
+    <?php get_footer(); ?>    
